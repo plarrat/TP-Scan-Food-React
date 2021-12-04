d35dd5f42958160dd4787fb69008726df0e48b0e
plarrat <larrat.philippe@lessonsharing.fr>
2021-12-05 00:12:38 +0100

    changement de l'URL open food facts

--- src/utils/ApiHook.jsx
+++ src/utils/ApiHook.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function getProduct(ean){
     return new Promise((resolve,reject)=>{
         if(ean === undefined) reject("No EAN provided");
-        axios.get(`https://world.openfoodfacts.org/api/v0/product/${ean}.json`)
+        axios.get(`https://fr.openfoodfacts.org/api/v0/product/${ean}.json`)
         .then(datas=>resolve(datas));
     })
 }
